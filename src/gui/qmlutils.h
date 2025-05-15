5604e19a3f1a8e9d1d7dedc6a6dcf198fe357469
Hannah von Reth <h.vonreth@opencloud.eu>
2025-05-15 14:32:07 +0200

    Convert new url dialog to an 'account modal dialog'

--- src/gui/qmlutils.h
+++ src/gui/qmlutils.h
@@ -29,10 +29,6 @@ public:                                                                         
         focusPreviousChild();                                                                                                                                  \
     }                                                                                                                                                          \
                                                                                                                                                                \
-Q_SIGNALS:                                                                                                                                                     \
-    void focusFirst();                                                                                                                                         \
-    void focusLast();                                                                                                                                          \
-                                                                                                                                                               \
 private:
 
 namespace OCC::QmlUtils {
@@ -61,6 +57,6 @@ protected:
     bool event(QEvent *event) override;
 
 private:
-    QWidget *_parentFocusWidget;
+    QWidget *_parentFocusWidget = nullptr;
 };
 }
