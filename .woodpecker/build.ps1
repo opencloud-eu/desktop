cdc761c7ceeac64d9fd950d22eea64182160b17b
Michael 'Flimmy' Flemming <m.flemming@opencloud.eu>
2025-07-29 11:04:15 +0200

    testing pipeline

--- .woodpecker/build.ps1
+++ .woodpecker/build.ps1
@@ -1,4 +1,5 @@
-New-Item -itemtype Junction -path $HOME/craft -value $env:CI_WORKSPACE/woodpecker_craft;
+git config --global --add safe.directory *
+New-Item -itemtype Junction -path $HOME/craft -value $env:CI_WORKSPACE/woodpecker_craft
 New-Item -Path $HOME/cache -ItemType Directory -ErrorAction SilentlyContinue
 .github/workflows/.craft.ps1 --setup
 .github/workflows/.craft.ps1 -c --unshelve .craft.shelf
