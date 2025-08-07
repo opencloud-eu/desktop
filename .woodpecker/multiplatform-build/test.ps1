39b441256f8d705f6073bdec76de040b9bf80ee8
Michael 'Flimmy' Flemming <m.flemming@opencloud.eu>
2025-08-07 09:02:17 +0200

    pipeline test

---
+++ .woodpecker/multiplatform-build/test.ps1
@@ -0,0 +1,13 @@
+# Enforce Strict Mode to Prevent weird variable behavior
+Set-StrictMode -Version Latest;
+
+# exit on errors, and suppress status display
+$ErrorActionPreference = 'Stop';
+$PSNativeCommandUseErrorActionPreference = $true;
+$ProgressPreference = 'SilentlyContinue';
+
+.woodpecker/multiplatform-build/init.ps1
+
+.github/workflows/.craft.ps1 -c --no-cache --test opencloud/opencloud-desktop
+
+exit 0
