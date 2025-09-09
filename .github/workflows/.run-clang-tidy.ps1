6a34d2a464fc066624a2541da09687bc6e60b306
Hannah von Reth <h.vonreth@opencloud.eu>
2025-09-09 14:40:29 +0200

    Add clazy tests

--- .github/workflows/.run-clang-tidy.ps1
+++ .github/workflows/.run-clang-tidy.ps1
@@ -1,1 +1,15 @@
-run-clang-tidy -p "$env:BUILD_DIR" | Tee-Object -Path "$([System.IO.Path]::GetTempPath())/clang-tidy.log"
+$clazy="ClazyClangTidy"
+
+if($isWindows) {
+    $clazy += ".dll"
+} elseif($isLinux) {
+    $clazy = "${env:KDEROOT}/lib/${clazy}.so"
+} else
+{
+    $clazy = "${env:KDEROOT}/lib/${clazy}.dylib"
+}
+$CLAZY_LEVEL0="clazy-overloaded-signal,clazy-connect-by-name,clazy-connect-non-signal,clazy-qstring-comparison-to-implicit-char,clazy-wrong-qevent-cast,clazy-lambda-in-connect,clazy-lambda-unique-connection,clazy-qdatetime-utc,clazy-qgetenv,clazy-qstring-insensitive-allocation,clazy-fully-qualified-moc-types,clazy-unused-non-trivial-variable,clazy-connect-not-normalized,clazy-mutable-container-key,clazy-qenums,clazy-qmap-with-pointer-key,clazy-qstring-ref,clazy-strict-iterators,clazy-writing-to-temporary,clazy-container-anti-pattern,clazy-qcolor-from-literal,clazy-qfileinfo-exists,clazy-qstring-arg,clazy-empty-qstringliteral,clazy-qt-macros,clazy-temporary-iterator,clazy-wrong-qglobalstatic,clazy-lowercase-qml-type-name,clazy-no-module-include,clazy-use-static-qregularexpression"
+$CLAZY_LEVEL1="clazy-auto-unexpected-qstringbuilder,clazy-connect-3arg-lambda,clazy-const-signal-or-slot,clazy-detaching-temporary,clazy-foreach,clazy-incorrect-emit,clazy-install-event-filter,clazy-non-pod-global-static,clazy-post-event,clazy-qdeleteall,clazy-qlatin1string-non-ascii,clazy-qproperty-without-notify,clazy-qstring-left,clazy-range-loop-detach,clazy-range-loop-reference,clazy-returning-data-from-temporary,clazy-rule-of-two-soft,clazy-child-event-qobject-cast,clazy-virtual-signal,clazy-overridden-signal,clazy-qhash-namespace,clazy-skipped-base-method,clazy-readlock-detaching"
+$CLAZY_LEVEL2="clazy-ctor-missing-parent-argument,clazy-base-class-event,clazy-copyable-polymorphic,clazy-function-args-by-ref,clazy-function-args-by-value,clazy-global-const-char-pointer,clazy-implicit-casts,clazy-missing-qobject-macro,clazy-missing-typeinfo,clazy-old-style-connect,clazy-qstring-allocations,clazy-returning-void-expression,clazy-rule-of-three,clazy-virtual-call-ctor,clazy-static-pmf"
+
+run-clang-tidy -load="${clazy}" -checks="${CLAZY_LEVEL0},-overloaded-signal,qt-keywords" -p "$env:BUILD_DIR" | Tee-Object -Path "$([System.IO.Path]::GetTempPath())/clang-tidy.log"
