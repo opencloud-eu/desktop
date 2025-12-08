7277260839a2b3e389a2d2c20bece26bede2afad
Hannah von Reth <h.vonreth@opencloud.eu>
2025-12-08 10:34:51 +0100

    Apply syntax suggested by copilot

--- admin/clean-leftovers.ps1
+++ admin/clean-leftovers.ps1
@@ -2,6 +2,17 @@ Param
 (
   [string]$ProductName="OpenCloud"
 )
+<#
+.SYNOPSIS
+    Cleans up leftover Windows VFS sync roots and CLSID entries.
+.DESCRIPTION
+    Removes registry entries for specified product and test artifacts.
+    WARNING: This script will restart Windows Explorer.
+.PARAMETER ProductName
+    The product name to clean up. Defaults to "OpenCloud".
+.NOTES
+    Requires Administrator privileges.
+#>
 
 # OC-TEST is used by our unit tests
 # https://github.com/MicrosoftDocs/winrt-api/issues/1130
@@ -9,7 +20,7 @@ $SyncRootManager = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Syn
 $GetRootItem = Get-Item $SyncRootManager
 Get-ChildItem -Path  $SyncRootManager | ForEach-Object {
     $name = $_.Name.Substring($GetRootItem.Name.Length + 1)
-    if ($name.StartsWith("OC-TEST", "CurrentCultureIgnoreCase") -or $name.StartsWith("$ProductName", "CurrentCultureIgnoreCase")) {
+    if ($name.StartsWith("OC-TEST", [System.StringComparison]::CurrentCultureIgnoreCase) -or $name.StartsWith("$ProductName", [System.StringComparison]::CurrentCultureIgnoreCase)) {
         Write-Host "Removing sync root: ${name} `"${_}`""
         Remove-Item -Recurse $_.PsPath 
     }
@@ -18,7 +29,7 @@ Get-ChildItem -Path  $SyncRootManager | ForEach-Object {
 Get-ChildItem -Path "HKCU:\Software\Classes\CLSID\"  | ForEach-Object {
     $key = (get-itemproperty $_.PsPath)."(default)"
     if ($key) {
-        if ($key.StartsWith("OC-TEST", "CurrentCultureIgnoreCase") -or $key.StartsWith("$ProductName", "CurrentCultureIgnoreCase")) {
+        if ($key.StartsWith("OC-TEST", [System.StringComparison]::CurrentCultureIgnoreCase) -or $key.StartsWith("$ProductName", [System.StringComparison]::CurrentCultureIgnoreCase)) {
             Write-Host "Removing: ${key} `"${_}`""
             Remove-Item -Recurse $_.PsPath
         }
