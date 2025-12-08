472e900b77dde5a23e08bac55060f2c5ec15ca40
Hannah von Reth <h.vonreth@opencloud.eu>
2025-12-08 10:20:02 +0100

    Improve log message

--- admin/clean-leftovers.ps1
+++ admin/clean-leftovers.ps1
@@ -1,6 +1,5 @@
 Param
 (
-  [Parameter(Mandatory=$true)]
   [string]$ProductName="OpenCloud"
 )
 
@@ -11,7 +10,7 @@ $GetRootItem = Get-Item $SyncRootManager
 Get-ChildItem -Path  $SyncRootManager | ForEach-Object {
     $name = $_.Name.Substring($GetRootItem.Name.Length + 1)
     if ($name.StartsWith("OC-TEST", "CurrentCultureIgnoreCase") -or $name.StartsWith("$ProductName", "CurrentCultureIgnoreCase")) {
-        Write-Host $_
+        Write-Host "Removing sync root: ${name} `"${_}`""
         Remove-Item -Recurse $_.PsPath 
     }
 }
@@ -20,7 +19,7 @@ Get-ChildItem -Path "HKCU:\Software\Classes\CLSID\"  | ForEach-Object {
     $key = (get-itemproperty $_.PsPath)."(default)"
     if ($key) {
         if ($key.StartsWith("OC-TEST", "CurrentCultureIgnoreCase") -or $key.StartsWith("$ProductName", "CurrentCultureIgnoreCase")) {
-            Write-Host $key, $_
+            Write-Host "Removing: ${key} `"${_}`""
             Remove-Item -Recurse $_.PsPath
         }
     }
