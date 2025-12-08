ba6199a5a68200d514c897e80fbb399d0f1c7279
Hannah von Reth <h.vonreth@opencloud.eu>
2025-12-08 10:09:40 +0100

    Fix branding

--- admin/clean-leftovers.ps1
+++ admin/clean-leftovers.ps1
@@ -1,17 +1,16 @@
 Param
 (
   [Parameter(Mandatory=$true)]
-  [string]$ProductName
+  [string]$ProductName="OpenCloud"
 )
 
+# OC-TEST is used by our unit tests
 # https://github.com/MicrosoftDocs/winrt-api/issues/1130
 $SyncRootManager = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\SyncRootManager"
 $GetRootItem = Get-Item $SyncRootManager
 Get-ChildItem -Path  $SyncRootManager | ForEach-Object {
-    # legacy clients always used that prefix owncloud_
-
     $name = $_.Name.Substring($GetRootItem.Name.Length + 1)
-    if ($name.StartsWith("OC-TEST", "CurrentCultureIgnoreCase") -or $name.StartsWith("owncloud_", "CurrentCultureIgnoreCase") -or $name.StartsWith("$ProductName", "CurrentCultureIgnoreCase")) {
+    if ($name.StartsWith("OC-TEST", "CurrentCultureIgnoreCase") -or $name.StartsWith("$ProductName", "CurrentCultureIgnoreCase")) {
         Write-Host $_
         Remove-Item -Recurse $_.PsPath 
     }
@@ -20,7 +19,6 @@ Get-ChildItem -Path  $SyncRootManager | ForEach-Object {
 Get-ChildItem -Path "HKCU:\Software\Classes\CLSID\"  | ForEach-Object {
     $key = (get-itemproperty $_.PsPath)."(default)"
     if ($key) {
-        # unit tests (OC-TEST) leave stuff behind
         if ($key.StartsWith("OC-TEST", "CurrentCultureIgnoreCase") -or $key.StartsWith("$ProductName", "CurrentCultureIgnoreCase")) {
             Write-Host $key, $_
             Remove-Item -Recurse $_.PsPath
