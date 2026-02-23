4e6188a9918b23661285507381486ea094f0f54e
Saw-jan <saw.jan.grg3e@gmail.com>
2026-02-23 14:41:30 +0545

    ci: use latest rolling opencloud server version
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- test/gui/woodpecker/script.sh
+++ test/gui/woodpecker/script.sh
@@ -1,35 +1,6 @@
 #!/bin/bash
 
-source .woodpecker.env
-
-# Function to get the latest OpenCloud commit ID
-get_latest_opencloud_commit_id() {
-    echo "Getting latest commit ID for branch: $OPENCLOUD_BRANCH"
-    latest_commit_id=$(git ls-remote https://github.com/opencloud-eu/opencloud.git "refs/heads/$OPENCLOUD_BRANCH" | cut -f 1)
-
-    # Update the OPENCLOUD in the .woodpecker.env file
-    env_file="./.woodpecker.env"
-    sed -i "s/^OPENCLOUD_COMMITID=.*/OPENCLOUD_COMMITID=$latest_commit_id/" "$env_file"
-
-    echo "Updated .woodpecker.env with latest commit ID: $latest_commit_id"
-    cat $env_file
-    exit 0
-}
-
-# Function to check if the cache exists for the given commit ID
-check_opencloud_cache() {
-    echo "Checking OpenCloud cache for commit ID: $OPENCLOUD_COMMITID"
-    opencloud_cache=$(mc find s3/$CACHE_BUCKET/opencloud-build/$OPENCLOUD_COMMITID/opencloud 2>&1 | grep 'Object does not exist')
-
-    if [[ "$opencloud_cache" != "" ]]
-    then
-        echo "$OPENCLOUD_COMMITID doesn't exist in cache."
-        ENV="OPENCLOUD_CACHE_FOUND=false\n"
-    else
-      echo "$OPENCLOUD_COMMITID found in cache."
-      ENV="OPENCLOUD_CACHE_FOUND=true\n"
-    fi
-}
+touch .woodpecker.env
 
 # get playwright version from package.json
 get_playwright_version() {
@@ -80,8 +51,6 @@ check_python_cache() {
 if [[ "$1" == "" ]]; then
     echo "Usage: $0 [COMMAND]"
     echo "Commands:"
-    echo -e "  get_latest_opencloud_commit_id \t get the latest OpenCloud commit ID"
-    echo -e "  check_opencloud_cache \t\t check if the cache exists for the given commit ID"
     echo -e "  get_playwright_version \t get the playwright version from package.json"
     echo -e "  check_browsers_cache \t check if the browsers cache exists for the given playwright version"
     echo -e "  check_python_cache \t check if a cache for the current requirements.txt exists"
