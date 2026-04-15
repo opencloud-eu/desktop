8b2092f7f9f4909ad11325eacc83a4b02ade264e
Sajan Gurung <saw.jan.grg3e@gmail.com>
2026-04-15 17:29:17 +0545

    test: fix config path
    Signed-off-by: Sajan Gurung <saw.jan.grg3e@gmail.com>

--- test/gui/environment.py
+++ test/gui/environment.py
@@ -18,10 +18,6 @@ def before_scenario(context, feature):
 
 
 def after_scenario(context, scenario):
-    # clean up config dir
-    confi_dir = get_config("clientConfigDir")
-    if os.path.exists(confi_dir):
-        shutil.rmtree(confi_dir)
     # clean up sync dir
     for entry in os.scandir(get_config("clientRootSyncPath")):
         try:
