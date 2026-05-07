e1d749a994d0bcae38a40183a9d555f2750127ea
Sawjan Gurung <saw.jan.grg3e@gmail.com>
2026-05-07 12:33:50 +0545

    ci: run appium gui tests (#891)
    * test: fix make targets
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * ci: run appium GUI tests
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * ci: use workspace env
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * ci: fix cache path
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * ci: use dot to source file
    
    * ci: install browsers
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * ci: run GUI tests
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * ci: run GUI tests
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * ci: provide behave test dir
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * test: do not initialize socket connection
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * ci: run with verbose
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * ci: update ui-tests.yaml
    
    * ci: run a scenario
    
    * ci: run webdriver-atspi server
    
    * ci: update webdriver url
    
    * ci: fix path
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * test: use script to run the webdriver
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * ci: run all tests
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * ci: run failing tests
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * ci: fix cache
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * ci: run all smoke tests
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * ci: update build image
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    ---------
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

---
+++ test/gui/woodpecker/run_atspi_webdriver.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+TEST_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../ && pwd)"
+WEBDRIVER_DIR="$TEST_DIR/__webdriver"
+
+mkdir -p "$WEBDRIVER_DIR"
+
+DRIVER_FILE="atspi-webdriver.py"
+DRIVER_URL="https://raw.githubusercontent.com/KDE/selenium-webdriver-at-spi"
+
+# shellcheck disable=SC1091
+. "$TEST_DIR/.woodpecker.env"
+
+if [ -z "$ATSPI_WEBDRIVER_VERSION" ]; then
+    ATSPI_WEBDRIVER_VERSION="master"
+fi
+
+if [ ! -f "$WEBDRIVER_DIR/$DRIVER_FILE" ]; then
+    curl -sSL --fail "$DRIVER_URL/$ATSPI_WEBDRIVER_VERSION/selenium-webdriver-at-spi.py" -o "$WEBDRIVER_DIR/$DRIVER_FILE"
+fi
+
+if [ ! -f "$WEBDRIVER_DIR/app_roles.py" ]; then
+    curl -sSL --fail "$DRIVER_URL/$ATSPI_WEBDRIVER_VERSION/app_roles.py" -o "$WEBDRIVER_DIR/app_roles.py"
+fi
+
+if [ -z "$WEBDRIVER_HOST" ]; then
+    WEBDRIVER_HOST="0.0.0.0"
+fi
+if [ -z "$WEBDRIVER_PORT" ]; then
+    WEBDRIVER_PORT="4723"
+fi
+
+# run webdriver server
+export FLASK_ENV=production
+export FLASK_APP="$WEBDRIVER_DIR/$DRIVER_FILE"
+flask run --host="$WEBDRIVER_HOST" --port="$WEBDRIVER_PORT" --no-reload
